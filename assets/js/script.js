d49127bcdb4eff40b96c970315915535cf729ece
wanjiiru <liz240@live.com>
2018-04-30 01:41:09 -0500

    add chnages to footer sextion

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,24 +15,3 @@ function openPage(pageName,elmnt,color) {
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
-
-
-
-$(document).ready(function () {
-    $(".fa-bars").click(function () {
-        document.getElementById("big-menu").style.width = "100%";
-        $("#navigation").hide();
-    });
-    $(".closebtn").click(function () {
-        document.getElementById("big-menu").style.width = "0%";
-        $("#navigation").show();
-    });
-
-    (function ($) {
-        window.fnames = new Array();
-        window.ftypes = new Array();
-        fnames[0] = 'EMAIL';
-        ftypes[0] = 'email';
-    }(jQuery));
-    var $mcj = jQuery.noConflict(true);
-});
